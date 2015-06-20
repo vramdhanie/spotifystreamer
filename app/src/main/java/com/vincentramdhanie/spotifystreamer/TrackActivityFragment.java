58147cc38d4ecf92703c9d9eadd49a4a3384ebf0
Vincent Ramdhanie <vramdhanie@gmail.com>
2015-06-20 11:10:08 -0400

    Add a new activity for Tracks

---
+++ app/src/main/java/com/vincentramdhanie/spotifystreamer/TrackActivityFragment.java
@@ -0,0 +1,23 @@
+package com.vincentramdhanie.spotifystreamer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TrackActivityFragment extends Fragment {
+
+    public TrackActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_track, container, false);
+    }
+}
